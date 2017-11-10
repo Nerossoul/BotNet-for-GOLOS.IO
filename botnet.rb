aa7edbe1094853c0698e1058976537522c7c7428
Nerossoul <nerossoul@gmail.com>
2017-11-10 10:07:27 +0700

    working tranaction methods

--- botnet.rb
+++ botnet.rb
@@ -1,13 +1,18 @@
 require_relative 'golosuser.rb'
+# BotNet is keeping golos users array and drive each over
+#
 class BotNet
 
   attr_reader(:users)
 
   def initialize
     @users = []
+    # create_users
   end
 
-  def read_users_data_from_file(file_name)
+  def read_users_data_from_file
+    current_path = File.dirname(__FILE__)
+    file_name = current_path + '/botlist.txt'
     if File.exist?(file_name)
       f = File.new(file_name, 'r:UTF-8')
       users_array = f.readlines
@@ -15,52 +20,89 @@ class BotNet
       users_hashs = []
       users_array.each do |user_data|
         user_auth = user_data.split
-        users_hashs << { user_name: user_auth[0], post_key: user_auth[1], activ_key: user_auth[2] }
+        users_hashs << {
+          user_name: user_auth[0],
+          post_key: user_auth[1],
+          activ_key: user_auth[2]
+        }
       end
-      users_array = nil
-      users_hashs
+      return users_hashs
     else
       puts "file #{file_name} not found"
-      nil
     end
   end
 
-  def create_users(user_hashs)
+  def create_users
+    user_hashs = read_users_data_from_file
     user_hashs.each do |user|
-      @users << GolosUser.new(user[:user_name],user[:post_key], user[:activ_key])
+      @users << GolosUser.new(user[:user_name], user[:post_key], user[:activ_key])
     end
   end
 
   def get_lust_post_data(user_name)
     lust_post_data = []
     max_number = get_account_history_max_number(user_name)
+    limit_response = 1999
     api = Radiator::Api.new(chain: :golos, url: 'https://ws.golos.io')
-
     while max_number > 0 do
       puts "запрос в блокчейн"
-      response = api.get_account_history(user_name, max_number, 1999)
-      puts JSON.pretty_generate(response)
-
+      limit_response = max_number - 1 if limit_response > max_number
+      response = api.get_account_history(user_name, max_number, limit_response)
+      #puts JSON.pretty_generate(response)
       response['result'].reverse_each do |result|
         if result[1]['op'][0] == 'comment' then
           if (result[1]['op'][1]['author'] == user_name && result[1]['op'][1]['parent_author'] == '') then
             lust_post_data = result[1]
             max_number = 0
+            break
           end
         end
       end
       max_number = max_number - 2000
       sleep(1)
     end
-
     return lust_post_data
   end
 
   def get_account_history_max_number(user_name)
     api = Radiator::Api.new(chain: :golos, url: 'https://ws.golos.io')
-    response = api.get_account_history(user_name, 800000000000, 0)
+    response = api.get_account_history(user_name, 800_000_000_000, 0)
     max_number = response['result'][0][0]
     sleep(1)
     return max_number
   end
-end
+
+  def sign_transaction(transaction_data, wif_key)
+    tx = Radiator::Transaction.new(wif: wif_key, chain: :golos, url: 'https://ws.golos.io')
+    puts transaction_data
+    tx.operations << transaction_data
+    response = tx.process(true)
+    puts JSON.pretty_generate(response)
+  end
+
+  def create_vote_data(voter, author, permlink, weight)
+    vote = {
+      type: :vote,
+      voter: voter,
+      author: author,
+      permlink: permlink,
+      weight: weight.to_i
+    }
+    return vote
+  end
+
+  def create_comment_data(parent_permlink, author, permlink, title, body, json_metadata, parent_author)
+    comment = {
+      type: :comment,
+      parent_permlink: parent_permlink,
+      author: author,
+      permlink: permlink,
+      title: title,
+      body: body,
+      json_metadata: json_metadata,
+      parent_author: parent_author
+    }
+    return comment
+  end
+
+end #class end
--- start.rb
+++ start.rb
@@ -1,19 +1,10 @@
-# запуск работы botnet
+# start botnet and go to rest
 require_relative 'botnet.rb'
 require_relative 'golosuser.rb'
 
-
-
-current_path = File.dirname(__FILE__)
-file_name = current_path + "/botlist.txt"
-
 botnet_commander = BotNet.new
 
-#users_data_array = botnet_commander.read_users_data_from_file(file_name)
+#puts JSON.pretty_generate(botnet_commander.get_lust_post_data('golos.loto'))
 
-#botnet_commander.create_users(users_data_array)
-
-
-
-puts JSON.pretty_generate(botnet_commander.get_lust_post_data('pugovizza'))
-
+vote = botnet_commander.create_comment_data(parent_permlink, author, permlink, title, body, json_metadata, parent_author)
+botnet_commander.sign_transaction(vote, '5KWtX3UPCY6qvuxpYabZPPwiZ7weK3bstSGXyTjWcDkkkXgWjSX')
